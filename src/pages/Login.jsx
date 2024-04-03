6d8371dc51b228eddcb7765cfd0b83cbbe5f5b07
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-04-03 00:51:51 -0600

    upd

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import LoginForm from "../components/Forms/LoginForm/LoginForm";
+// import * as React from "react";
+// import Button from "@mui/material/Button";
 
 const Login = () => {
   return (
     <>
       <LoginForm />
-      <Link to="/register">Or register</Link>
+      {/* <Button variant="contained"> */}
+        <Link to="/register">Or register</Link>
+      {/* </Button> */}
     </>
   );
 };
