2c1bee8f9701855cef54f55f588ce087dc6cb04e
Escbrk <69437294+Escbrk@users.noreply.github.com>
2024-07-07 01:51:49 -0600

    upd

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,7 +3,7 @@ import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import Navigation from "../Navigation/Navigation";
 import AuthNav from "../AuthNav/AuthNav";
 import UserMenu from "../UserMenu/UserMenu";
-import  './AppBar.module.css'
+import './AppBar.module.css'
 
 const AppBar = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
